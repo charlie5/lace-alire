a4fc5753f702613b5504f8d65de1d2546247c884
Rod Kay <rodakay5@gmail.com>
2022-07-31 15:20:41 +1000

    lace.text.utility: Add more documentation for 'replace' subprograms.

--- 1-base/lace/source/text/lace-text-utility.ads
+++ 1-base/lace/source/text/lace-text-utility.ads
@@ -9,11 +9,13 @@ is
    function  replace  (Self : in     Text.item;   Pattern : in String;
                                                   By      : in String) return Text.item;
    --
+   -- Replaces all occurences of 'Pattern' with 'By'.
    -- If the replacement exceeds the capacity of 'Self', the result will be expanded.
 
    procedure replace  (Self : in out Text.item;   Pattern : in String;
                                                   By      : in String);
    --
+   -- Replaces all occurences of 'Pattern' with 'By'.
    -- 'Text.Error' will be raised if the replacement exceeds the capacity of 'Self'.
 
 end lace.Text.utility;
